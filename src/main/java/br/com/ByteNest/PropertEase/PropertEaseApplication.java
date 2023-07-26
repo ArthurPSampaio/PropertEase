c7b445fbcda5fc2fb10b6e783258a29292f567ed
ArthurPSampaio <113519439+ArthurPSampaio@users.noreply.github.com>
2023-07-26 09:33:08 -0300

    First commit

---
+++ src/main/java/br/com/ByteNest/PropertEase/PropertEaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.ByteNest.PropertEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ByteNest/PropertEase/PropertEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ByteNest.PropertEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
