247ae684968c3734e921ced7cdc0d778baa4312e
Yago Daoud <yagodaoud@hotmail.com>
2023-07-28 09:48:36 -0300

    :sparkles: Feat: Create ClientController, ClientRepository and RegisterClientData

---
+++ src/main/java/br/com/ByteNest/PropertEase/controllers/ClientController.java
@@ -0,0 +1,33 @@
+package br.com.ByteNest.PropertEase.controllers;
+
+
+import br.com.ByteNest.PropertEase.models.clients.RegisterClientData;
+import br.com.ByteNest.PropertEase.models.clients.Client;
+import br.com.ByteNest.PropertEase.models.clients.ClientRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("clients")
+public class ClientController {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<String> register(@RequestBody @Validated RegisterClientData data){
+        Client client = new Client(data);
+        repository.save(client);
+
+        return new ResponseEntity<> ("Client created", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/br/com/ByteNest/PropertEase/models/clients/ClientRepository.java
@@ -0,0 +1,9 @@
+package br.com.ByteNest.PropertEase.models.clients;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+
+}
