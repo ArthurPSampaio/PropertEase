485da6a6bd59ac3db51e4c8b82cad2edadb5b16f
ArthurPSampaio <113519439+ArthurPSampaio@users.noreply.github.com>
2023-07-28 23:29:04 -0300

    :pencil2: Fix: fixed Schedules entity file

--- src/main/java/br/com/ByteNest/PropertEase/models/schedules/Schedules.java
+++ src/main/java/br/com/ByteNest/PropertEase/models/schedules/Schedules.java
@@ -1,26 +1,31 @@
 package br.com.ByteNest.PropertEase.models.schedules;
 
 import br.com.ByteNest.PropertEase.models.clients.Client;
+import br.com.ByteNest.PropertEase.models.employees.Employee;
 import br.com.ByteNest.PropertEase.models.realty.Realty;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.experimental.PackagePrivate;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
-@Table(name = "Visits")
+@Table(name = "Schedules")
 @Data
 public class Schedules {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "id")
+    @ManyToOne
+    @PrimaryKeyJoinColumn(name = "employee_id", referencedColumnName = "id")
+    private Employee employeeId;
+    @ManyToOne
+    @PrimaryKeyJoinColumn(name = "clients_id", referencedColumnName = "id")
     private Client clientId;
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "id")
+    @ManyToOne
+    @PrimaryKeyJoinColumn(name = "realty_id", referencedColumnName = "id")
     private Realty realtyId;
     private Date scheduled;
 }
